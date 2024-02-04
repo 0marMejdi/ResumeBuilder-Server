2f11821d8fe13fd8fc533c39f300b2f095ea2bd7
Ahmed Harabi <harabiahmed88@gmail.com>
2024-02-04 02:37:19 +0100

    fixed the tables creation's queries and updated all the functions

--- src/Repositories/UserRepository.js
+++ src/Repositories/UserRepository.js
@@ -5,27 +5,72 @@ let users = [{
     "lastName": "mejdi"
 }];
 function getUserById(id) {
-    return users.find(user => user.id === id);
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM user WHERE id = ?', [id], (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
 }
 
 function createUser(user) {
+        connection.query('INSERT INTO user SET ?',newUser, (err, result) => {
+        if (err) {
+         return('Error:', err);
+        } else {
+          return('Result:', result);
+        }
+      });
+    }
 
-    users.push(user);
 
+function updateUser(user) {
+    connection.query('UPDATE user SET ? WHERE id = ? ',[userUpdate,userUpdate.userID],(err,result)=>{
+        if (err) {
+            return('Error:', err);
+           } else {
+             
+            return('Result:', result);
+           }
+        
+    })
 }
-function updateUser(user) {}
 function getUserByEmail(email) {
-    return users.find(user => user.email === email);
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM user WHERE email = ?', [email], (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
 }
 function emailExists(email) {
-    //returns true if email exists, false otherwise
-    return !!users.find(user => user.email === email);
+    connection.query(`SELECT * FROM user WHERE email = ?`,[email],(err,result)=>{
+        if(err) throw new Error(err)
+        else if(result.legth==0){
+                return false; }
+        else{
+            return true;
+        }
+    })
 
 }
 
 function userExists(id) {
-    //returns true if user exists, false otherwise
-    return !!users.find(user => user.id === id);
+    connection.query(`SELECT * FROM user WHERE id = ?`,[id],(err,result)=>{
+        if(err) throw new Error(err);
+        else if(result.legth==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    })
 
 }
 module.exports = { getUserById, createUser, updateUser, getUserByEmail, emailExists, userExists };
