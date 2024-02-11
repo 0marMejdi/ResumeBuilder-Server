c1a561cc6557276b8e3eb5a5a7ce2eac1f1f41f7
0marMejdi <124160661+0marMejdi@users.noreply.github.com>
2024-02-11 03:09:32 +0100

    improved the documentation page

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let apis = [];
+let apisProject = [];
+let apisUser = [];
+let apisTemplate = [];
+
 let methods = {
     get: 'btn-success',
     post: 'btn-warning',
@@ -8,11 +12,14 @@ let methods = {
 }
 
 class APIEndpoint {
-    _method;
-    _url;
+    _method="";
+    _url="";
     _iobj=null;
     _oobj=null;
-    _decsription;
+    _decsription="";
+    _oobjDesc="";
+    _iobjDesc="";
+    _urlDesc="";
 
 
     method = (_method) => {
@@ -35,74 +42,63 @@ class APIEndpoint {
         this._decsription = _desc;
         return this;
     }
-    static toggle = (index) => {
-        const apiElement = document.getElementById(`API${index}`);
-        console.log("we are inside");
-        apiElement.classList.toggle('hide');
-        if (apiElement.innerHTML){
-            apiElement.innerHTML="";
-        }else{
-            apiElement.innerHTML=APIEndpoint.renderPartial(index);
-        }
-    }
-    static renderApi = (index)=>{
-        let html="";
-        let api = apis[index];
-        html += `<div class="container m-3">`;
-        html += `<span class="${methods[api._method]}">${api._method.toUpperCase()}</span> `;
-        html += `<span>${api._url}: </span>`;
-        html += `<div class="btn btn-outline-dark" onclick="APIEndpoint.toggle(${index})">Toggle</div>`;
-
-        html += APIEndpoint.renderPartial(index)
-        html += `</div>`;
-        html += `<hr>`;
-        return html;
-    }
-    static renderPartial=(index)=>{
-        let html="";
-        let api = apis[index];
-        // html += `<hr>`;
-        html += `<div id="API${index}" >`;
-        html += `<div class="description">${api._decsription}</div>`;
-        html += `<div>Input Object : </div>`
-        html += generateCodeMarkup(api._iobj);
-        html += `<div>Output Object : </div>`
-
-        html += generateCodeMarkup(api._oobj);
-        html += `</div>`;
-
-
-        return html;
-
-}
-    static renderAll = () => {
-        let html = '';
-        apis.forEach((api, index) => {
-            html+=APIEndpoint.renderApi(index);
-        });
-        document.getElementById('api-container').innerHTML = html;
-        document.querySelectorAll("#btn.btn-dark").forEach(bt=>bt.click());
-    }
+    
     add = () => {
         apis.push(this);
     }
-}
+    outDesc = (_oobjDesc)=>{
+        this._oobjDesc = _oobjDesc;
+        return this;
+    }
+    inDesc = (_iobjDesc)=>{
+        this._iobjDesc = _iobjDesc;
+        return this;
+    }
+    urlDesc = (_urlDesc)=>{
+        this._urlDesc = _urlDesc;
+        return this;
+    }
 
+}
 function generateCodeMarkup(obj, indentLevel = 0) {
     const indent = '&nbsp;&nbsp;&nbsp;&nbsp;'.repeat(indentLevel);
+    let html = '<pre class="code m-0 p-0"><code>';
 
-    let html = '<pre class="code"><code>';
-
-    if (typeof obj === 'object' && obj !== null) {
+    if (Array.isArray(obj)) {
+        if (obj.length === 0) {
+            console.log("Taadet");
+            html += indent + '[]';
+        } else {
+        html += indent + '[';
+        if (obj.length > 0) {
+            html += '<br>';
+            obj.forEach((item, index, array) => {
+                html += generateCodeMarkup(item, indentLevel + 1);
+                if (index !== array.length - 1) {
+                    html += ',<br>';
+                }
+            });
+            html +=  indent;
+        }
+        html += ']';}
+    } else if (typeof obj === 'object' && obj !== null) {
         html += indent + '{<br>';
-        Object.entries(obj).forEach(([key, value], index, array) => {
+        const keys = Object.keys(obj);
+        keys.forEach((key, index) => {
+            const value = obj[key];
             html += indent + '&nbsp;&nbsp;&nbsp;&nbsp;<span class="property">"' + key + '"</span>: ';
-
             if (typeof value === 'object' && value !== null) {
-                if (Object.keys(value).length === 0) {
+                if (Array.isArray(value)) {
+                    if (value.length === 0) {
+                        html += '[]';
+                    } else {
+                        html +=  generateCodeMarkup(value, indentLevel + 1);
+                    }
+                }
+                else if (Object.keys(value).length === 0) {
                     html += '<span class="null">undefined</span>';
                 } else {
-                    html += '<br>' + generateCodeMarkup(value, indentLevel + 1);
+                    html +=generateCodeMarkup(value, indentLevel + 1);
                 }
             } else if (typeof value === 'string') {
                 html += '<span class="string">"' + value + '"</span>';
@@ -115,9 +111,7 @@ function generateCodeMarkup(obj, indentLevel = 0) {
             } else if (value === undefined) {
                 html += '<span class="null">undefined</span>';
             }
-
-
-            if (index !== array.length - 1) {
+            if (index !== keys.length - 1) {
                 html += ',<br>';
             } else {
                 html += '<br>';
@@ -132,11 +126,13 @@ function generateCodeMarkup(obj, indentLevel = 0) {
     return html;
 }
 
+ 
 // Example usage
 new APIEndpoint()
     .method('get')
     .url('/project/list')
-    .description('Returns a list of all projects that use has.')
+    .urlDesc('Gets a list of all projects that user has.')
+    .outDesc('The Returned object is an array of project objects, each containing only the basic information of the project (not detailled).')
     .inObj(null)
     .outObj([
         {
@@ -162,7 +158,7 @@ new APIEndpoint()
 new APIEndpoint()
     .method('post')
     .url('/project/info/{projectId}')
-    .description('Returns all related infos to the project with the given ID.')
+    .urlDesc('Returns all related infos to the project with the given ID.')
     .outObj({
         "id": "1706863054682",
         "userId": "1706821332398",
@@ -191,7 +187,7 @@ new APIEndpoint()
 new APIEndpoint()
     .method('patch')
     .url('/project/info/{projectId}')
-    .description('Updates a certain field of snapshot. ')
+    .urlDesc('Updates a certain field of snapshot. ')
     .inObj({
         "projectId":"1706822220158",
         "fieldValue":5,
@@ -222,8 +218,24 @@ function createUrl(index){
     //<span className="mx-3"> @Model.Url</span>
     let url = document.createElement('span');
     url.classList.add(['mx-3']);
-    url.innerHTML= apis[index]._url;
+    let urlString = apis[index]._url;
+    let newParts;
+    if (urlString.includes("{")){ // surround it with spans indicatinng its params, the span class is parametri
+
+        let parts = urlString.split("/");
+        console.log(parts);
+        newParts = parts.map(part=>{            
+            if (part.includes("{")){
+                return `<span class="parametri">${part}</span>`;
+            }else{
+                return part;
+            }
+        });
+        urlString = newParts.join("/");   }
+    
+    url.innerHTML=urlString ;
     return url;
+    
 }
 function createMoreButton(index){
 
@@ -258,26 +270,39 @@ function createMethod(index){
     //        <div class="btn btn-@color disabled">@Model.Method</div>
     let elem = document.createElement('div');
     elem.className="btn " + methods[apis[index]._method] +" disabled";
-    elem.innerHTML=apis[index]._method;
+    elem.innerHTML=apis[index]._method.toString().toUpperCase();
     return elem;
+}
+function createUrlDescription(index){
+    let elem = document.createElement('div');
+    elem.innerHTML = apis[index]._urlDesc;
+    return elem;
+
 }
 function createApiHead(index){
-
     let elem = document.createElement('div');
-    elem.classList.add('card-body','p-0','d-flex','justify-content-between');
+    elem.classList.add('card-header','p-1','d-flex','justify-content-between');
     elem.append(createMethodAndUrl(index));
     elem.append(createMoreButton(index));
+
     return elem;
 }
-function createBodyInputExample() {
+function createBodyInputExample(index) {
     const bodyInputDiv = document.createElement("div");
-    bodyInputDiv.innerText = "Expected Body Input as Example:";
+    const requestWord = document.createElement("div");
+    requestWord.innerText = "Request Body:";
+    bodyInputDiv.append(requestWord);
+    bodyInputDiv.innerText += apis[index]._iobjDesc;
     return bodyInputDiv;
 }
 
-function createBodyOutputExample() {
+function createBodyOutputExample(index) {
     const bodyOutputDiv = document.createElement("div");
-    bodyOutputDiv.innerText = "Expected Body Output as Example:";
+    const responseWord = document.createElement("div");
+    responseWord.innerText = "Response Body:";
+    bodyOutputDiv.append(responseWord);
+    bodyOutputDiv.innerHTML += apis[index]._oobjDesc;
+    
     return bodyOutputDiv;
 }
 function getInput(index) {
@@ -299,39 +324,43 @@ function createApiBody(index){
     const div = document.createElement("div");
 
     // Set the class attribute
-    div.className = "collapse my-1";
+    div.className = "collapse my-1 p-2";
 
     // Set the id attribute
     div.id = id;
 
     let desc = document.createElement('div');
-    desc.innerHTML = apis[index]._decsription;
-    div.append(document.createElement('hr'));
+    if(apis[index]._decsription){desc.innerHTML = apis[index]._decsription;
+    div.append(document.createElement('hr'));}
     div.append(desc);
 
     if (apis[index]._iobj){
         div.append(document.createElement('hr'));
-        div.append(createBodyInputExample());
+        div.append(createBodyInputExample(index));
         div.append(getInput(index));
     }
     if (apis[index]._oobj){
         div.append(document.createElement('hr'));
-        div.append(createBodyOutputExample());
+        div.append(createBodyOutputExample(index));
         div.append(getOutput(index));
     }
     //div.append(getBodies(index));
-
-    return div;
+    let cardboyd= document.createElement('div');
+    cardboyd.className="card-body p-2";
+    cardboyd.append(createUrlDescription(index));
+    cardboyd.append(div);
+    return cardboyd;
 
 }
 function createApiCard(index){
     let apibody = document.createElement('div');
-    apibody.className="container mt-5";
+    apibody.className="container mt-4";
     let cardy  = document.createElement('div');
-    cardy.className='card p-2';
+    cardy.className='card';
     cardy.append(
        createApiHead(index)
     );
+    
     cardy.append(
         createApiBody(index)
     );
@@ -341,12 +370,16 @@ function createApiCard(index){
 }
 
 
-function insertAll(){
-    let cont = document.getElementById('api-container');
+function insertAll(elemId){
+    let cont = document.getElementById(elemId);
     apis.forEach((api,index)=>{
         let elem = createApiCard(index);
 
         cont.append(elem);
     })
 }
-insertAll();
+function insertAllDomains(){
+    
+    insertAll('api-container-project');
+}
+insertAllDomains();
